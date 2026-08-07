8c2c5864f3912879dcf873852b12dfabfbd46818
Đặng Quang Huy <quanghuy23052005@gmail.com>
2026-08-07 11:05:57 +0700

    Implement authentication
    1. Set up authentication using a starter kit (Jetstream, Breeze, etc.) provided by Laravel.
    2. Implement registration, login (using email and password), and logout functionality.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TaskController;
@@ -8,18 +9,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('users', UserController::class)->middleware('super_admin');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::resource('users', UserController::class)
+    ->middleware(['auth', 'super_admin']);
 
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-
 Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-
 Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-
 Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
-
 Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+require __DIR__.'/auth.php';
