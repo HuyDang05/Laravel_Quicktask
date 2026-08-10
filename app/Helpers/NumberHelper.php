a3979ba52185fdaa927c8ec989709b36bc3a6ccd
Đặng Quang Huy <quanghuy23052005@gmail.com>
2026-08-10 07:51:50 +0700

    Set up Sun* Lint

--- app/Helpers/NumberHelper.php
+++ app/Helpers/NumberHelper.php
@@ -3,6 +3,7 @@
 if (! function_exists('format_number_space')) {
     function format_number_space($number): string
     {
+        
         return number_format((float) $number, 0, '.', ' ');
     }
 }
